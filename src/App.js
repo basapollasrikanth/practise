b1e75f3a0dbd9da45e6330894ab3cdaa04f18808
basapollasrikanth <basapollasrikanth5613@gmail.com>
2023-09-05 23:03:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+function App() {     //component
+  useState();
+  return (
+    <div className="App">
+      <h1>Hello Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
